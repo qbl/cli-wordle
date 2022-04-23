ed4e66ed1a06cbec85eac9d16c5ce6b93d355e29
Iqbal Farabi <farabi.iqbal@gmail.com>
2022-04-23 01:58:48 +0000

    Update main, add game repl

--- main.rb
+++ main.rb
@@ -1,1 +1,11 @@
-puts "Hello, World!"
+require_relative 'lib/game'
+
+game = Game.new('SPEED')
+
+loop do
+  puts 'Your guess:'
+  guess_word = gets.chomp()
+  result = game.match_word(guess_word)
+  puts result
+  break if result == "\u{1F7E9}\u{1F7E9}\u{1F7E9}\u{1F7E9}\u{1F7E9}"
+end
